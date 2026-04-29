675fef1a6a2891d15c92171a0e3f18c234a105dd
zakaria <zakaria.22101005@gmail.com>
2026-04-29 16:19:37 +0100

    fix: résolution des conflits de fusion (doublons retirés)

--- transferly-backend/app/models/log.py
+++ transferly-backend/app/models/log.py
@@ -11,14 +11,3 @@ class Log(db.Model):
     details     = db.Column(db.Text, nullable=True)          # informations complémentaires libres
     date        = db.Column(db.DateTime, default=datetime.utcnow)
     user_id     = db.Column(db.Integer, db.ForeignKey('users.id'))
-from app.extensions import db
-from datetime import datetime
-
-class Log(db.Model):
-    __tablename__ = 'logs'
-
-    id = db.Column(db.Integer, primary_key=True)
-    action = db.Column(db.String(200), nullable=False)
-    statut = db.Column(db.String(20))
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
